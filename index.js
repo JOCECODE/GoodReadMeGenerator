ed22220bee5e4c1cca2835c8219cc43676d7866e
JOCECODE <66341350+JOCECODE@users.noreply.github.com>
2020-08-29 21:40:13 -0700

    added license badge choice

--- index.js
+++ index.js
@@ -12,14 +12,9 @@ const questions = [
   "Tests",
   "Questions",
 ];
-
+const licenseChoices = ["MIT", "GPLv3", "AGPL"];
 inquirer
   .prompt([
-    {
-      type: "input",
-      name: "userName",
-      message: questions[0],
-    },
     {
       type: "input",
       name: "title",
@@ -46,9 +41,10 @@ inquirer
       message: questions[5],
     },
     {
-      type: "input",
+      type: "list",
       name: "license",
       message: questions[6],
+      choices: licenseChoices,
     },
     {
       type: "input",
@@ -68,13 +64,22 @@ inquirer
   ])
   .then((userResponse) => {
     const data = writeHTML(userResponse);
-    fs.writeFile(`${userResponse.userName}.md`, data, function () {
+    fs.writeFile(`${userResponse.title}.md`, data, function () {
       console.log("success");
     });
   });
 
 function writeHTML(userObj) {
-  console.log("inside of the HTML FUNC", userObj);
+  if (userObj.license === licenseChoices[0]) {
+    userObj.license =
+      "[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)";
+  } else if (userObj.license === licenseChoices[1]) {
+    userObj.license =
+      "[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)";
+  } else {
+    userObj.license =
+      "[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0) ";
+  }
   return `
 # ${userObj.title}
 
