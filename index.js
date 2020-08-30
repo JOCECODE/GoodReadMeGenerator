f86bb240000584bee67fd5a8d4e030b000c9c298
JOCECODE <66341350+JOCECODE@users.noreply.github.com>
2020-08-30 11:46:12 -0700

    github username working

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+let licenseDes = "";
 const questions = [
-  "What is your name",
+  "What is your email address?",
   "What is the title of your project?",
   "Description: ",
   "Table Of Contents:",
@@ -10,16 +11,11 @@ const questions = [
   "License",
   "Contributing",
   "Tests",
-  "Questions",
+  "What's your GitHub username",
 ];
-
+const licenseChoices = ["MIT", "GPLv3", "AGPL"];
 inquirer
   .prompt([
-    {
-      type: "input",
-      name: "userName",
-      message: questions[0],
-    },
     {
       type: "input",
       name: "title",
@@ -46,9 +42,10 @@ inquirer
       message: questions[5],
     },
     {
-      type: "input",
+      type: "list",
       name: "license",
       message: questions[6],
+      choices: licenseChoices,
     },
     {
       type: "input",
@@ -62,43 +59,61 @@ inquirer
     },
     {
       type: "input",
-      name: "questions",
+      name: "github",
       message: questions[9],
+    },
+    {
+      type: "input",
+      name: "email",
+      message: questions[0],
     },
   ])
   .then((userResponse) => {
     const data = writeHTML(userResponse);
-    fs.writeFile(`${userResponse.userName}.md`, data, function () {
+    fs.writeFile(`${userResponse.title}.md`, data, function () {
       console.log("success");
     });
   });
 
 function writeHTML(userObj) {
-  console.log("inside of the HTML FUNC", userObj);
-  return `
-# ${userObj.title}
+  if (userObj.license === licenseChoices[0]) {
+    userObj.license =
+      "[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)";
+    licenseDes = "[MIT](https://choosealicense.com/licenses/mit/)";
+  } else if (userObj.license === licenseChoices[1]) {
+    userObj.license =
+      "[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)";
+    licenseDes = "[GPLv3](https://choosealicense.com/licenses/gpl-3.0/)";
+  } else {
+    userObj.license =
+      "[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0) ";
+    licenseDes = "[AGPL](https://choosealicense.com/licenses/gpl-3.0/)";
+  }
+  return `# ${userObj.title}
+  
+  ${userObj.license}
 
 ## Table of Contents
 ${userObj.tableOfCont}
 
 ## Installation
-
- ${userObj.installation}
+${userObj.installation}
 
 ## Usage
-
 ${userObj.usage}
 
 ## Contributing
 ${userObj.contributing}
 
 ## License
-${userObj.license}
-
+${licenseDes}
 
 ## Questions  
-${userObj.questions}
-  `;
+https://github.com/${userObj.github}  
+
+${userObj.email}
+
+Feel free to email me with any questions with the application or troubleshooting. Provide your name and or contact info and I will get back to you ASAP.`;
 }
 
 // .then(function (data) {
---
+++ ttt.md
@@ -0,0 +1,25 @@
+# ttt
+  
+  [![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+## Table of Contents
+lsakdfja
+
+## Installation
+laskdjf
+
+## Usage
+asldfjlkasd
+
+## Contributing
+laksdjf
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Questions  
+https://github.com/jocecode  
+
+ulah57@gmail.com
+
+Feel free to email me with any questions with the application or troubleshooting. Provide your name and or contact info and I will get back to you ASAP.
